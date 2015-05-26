a6fb3e970daf51e0bdbe7d235d62be2ac415d636
Dominique Luna <dluna132@gmail.com>
2015-05-26 18:52:18 -0400

    forgot to change perf.jl to the txt file

--- test/perf.jl
+++ test/perf.jl
@@ -1,19 +1,6 @@
 import GloVe
 
-corpus = split("""human interface computer
-survey user computer system response time
-eps user interface system
-system human system eps
-user response time
-trees
-graph trees
-graph minors trees
-graph minors survey
-I like graph and stuff
-I like trees and stuff
-Sometimes I build a graph
-Sometimes I build trees""", '\n')
-
+corpus = "data/corpus_test2.txt"
 vocab = GloVe.make_vocab(corpus)
 comatrix = GloVe.make_cooccur(vocab, corpus)
 model = GloVe.Model(comatrix, vecsize=100)
