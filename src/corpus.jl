fdeb3792e48d0090f5fc847ede816660b2ea29b6
Dominique Luna <dluna132@gmail.com>
2015-05-30 14:42:48 -0400

    changeS

--- src/corpus.jl
+++ src/corpus.jl
@@ -65,69 +65,15 @@ function make_vocab{T<:Token}(corpus::Vector{T})
     v
 end
 
-# make_cooccur creates the co-occurence matrix X. X is symmetric.
-# The window_size determines how many of the surrounding tokens should
-# be guaranteed as a context token to the main token.
-function make_cooccur(v::Vocab, filename::String; window_size::Int=10)
-    comatrix = spzeros(length(v), length(v))
-    open(filename) do f
-        for line = eachline(f)
-            tokens = split(line)
-            for i = 1:length(tokens)
-                @inbounds t = tokens[i]
-                c_id = v[t]
-
-                for li = max(1, i - window_size):i-1
-                    @inbounds lt = tokens[li]
-                    l_id = v[lt]
-
-                    # tokens that are d spaces spart, contribute
-                    # 1.0 / d to the total count.
-                    incr = 1.0 / (i - li)
-
-                    # symmetry
-                    @inbounds comatrix[c_id, l_id] += incr
-                    @inbounds comatrix[l_id, c_id] += incr
-                end
-            end
-        end
-    end
-    comatrix
-end
-
 immutable Pair
     i::Int
     j::Int
 end
 
 function make_cooccur{T<:Token}(v::Vocab, corpus::Vector{T}; window_size::Int=10)
-    comatrix = spzeros(length(v), length(v))
-    for i = 1:length(corpus)
-        @inbounds token = corpus[i]
-        c_id = v[token]
-
-        for li = max(1, i - window_size):i-1
-            @inbounds lt = corpus[li]
-            l_id = v[lt]
-
-            # tokens that are d spaces spart, contribute
-            # 1.0 / d to the total count.
-            incr = 1.0 / (i - li)
-
-            # symmetry
-            @inbounds comatrix[c_id, l_id] += incr
-            @inbounds comatrix[l_id, c_id] += incr
-        end
-        i % 10_000 == 0 && println("Done iteration ", i)
-    end
-    comatrix
-end
-
-function make_cooccur{T<:Token}(v::Vocab, corpus::Vector{T}; window_size::Int=10)
     comatrix = Dict{Pair, Float64}()
     for i = 1:length(corpus)
         @inbounds token = corpus[i]
-        println(token)
         c_id = v[token]
 
         for li = max(1, i - window_size):i-1
@@ -148,4 +94,59 @@ function make_cooccur{T<:Token}(v::Vocab, corpus::Vector{T}; window_size::Int=10
     comatrix
 end
 
+
+
+#= # make_cooccur creates the co-occurence matrix X. X is symmetric. =#
+#= # The window_size determines how many of the surrounding tokens should =#
+#= # be guaranteed as a context token to the main token. =#
+#= function make_cooccur(v::Vocab, filename::String; window_size::Int=10) =#
+#=     comatrix = spzeros(length(v), length(v)) =#
+#=     open(filename) do f =#
+#=         for line = eachline(f) =#
+#=             tokens = split(line) =#
+#=             for i = 1:length(tokens) =#
+#=                 @inbounds t = tokens[i] =#
+#=                 c_id = v[t] =#
+#=  =#
+#=                 for li = max(1, i - window_size):i-1 =#
+#=                     @inbounds lt = tokens[li] =#
+#=                     l_id = v[lt] =#
+#=  =#
+#=                     # tokens that are d spaces spart, contribute =#
+#=                     # 1.0 / d to the total count. =#
+#=                     incr = 1.0 / (i - li) =#
+#=  =#
+#=                     # symmetry =#
+#=                     @inbounds comatrix[c_id, l_id] += incr =#
+#=                     @inbounds comatrix[l_id, c_id] += incr =#
+#=                 end =#
+#=             end =#
+#=         end =#
+#=     end =#
+#=     comatrix =#
+#= end =#
+
+
+#= function make_cooccur{T<:Token}(v::Vocab, corpus::Vector{T}; window_size::Int=10) =#
+#=     comatrix = spzeros(length(v), length(v)) =#
+#=     for i = 1:length(corpus) =#
+#=         @inbounds token = corpus[i] =#
+#=         c_id = v[token] =#
+#=  =#
+#=         for li = max(1, i - window_size):i-1 =#
+#=             @inbounds lt = corpus[li] =#
+#=             l_id = v[lt] =#
+#=  =#
+#=             # tokens that are d spaces spart, contribute =#
+#=             # 1.0 / d to the total count. =#
+#=             incr = 1.0 / (i - li) =#
+#=  =#
+#=             # symmetry =#
+#=             @inbounds comatrix[c_id, l_id] += incr =#
+#=             @inbounds comatrix[l_id, c_id] += incr =#
+#=         end =#
+#=         i % 10_000 == 0 && println("Done iteration ", i) =#
+#=     end =#
+#=     comatrix =#
+#= end =#
 
